f8ad15ae8f6df4ff97682fa05fd35c00a909e225
Scotty <scotty@Scottys-Mac-Pro.local>
2016-09-06 22:46:38 -0700

    updated the link.sh for tern

--- link.sh
+++ link.sh
@@ -4,7 +4,9 @@ echo $TEST
 
 rm ../.tmux.conf 
 rm ../.vimrc 
+rm ../.tern-config 
 
+ln -s $TEST/vimrc ../.vimrc 
 ln -s $TEST/tmux.conf ../.tmux.conf 
-ln -s $TEST/vimrc ../.vimrc
+ln -s $TEST/tern-config ../.tern-config 
 
